1ed5b88b9be9630fe584baeee63674762ceb409b
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2024-02-25 15:56:30 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import "./App.css";
 import Convert from "./components/Convert";
 
 function App() {
- 
   return (
-   <Convert/>
+    <>
+      <Convert />
+      <footer>
+        Developed with ❤️ by {" "}
+        <a href="https://github.com/SudirKrishnaaRS">Sudir</a>
+      </footer>
+    </>
   );
 }
 
